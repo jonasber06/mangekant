e8c3ede0eee0fc255d53f255c2c0d1dd97a6f937
EspenTinius <tiniusespen@gmail.com>
2024-10-31 14:45:24 +0100

    addet ledetekst

--- mangekant.py
+++ mangekant.py
@@ -1,17 +1,21 @@
 class Mangekant:
+    """klasse for kor mykje sidar ein figur har med kor lang kvær sida er"""
     def __init__(self,antallKanter:int,lengde_li:list):
         self.antallKanter = antallKanter
         self.lengde_li = lengde_li
     
     def angiLengder(self):
+        """du kan sette in lengdene til figuren med denne funksjona"""
         print("")
         for i in range(self.antallKanter):
             self.lengde_li.append(int(input(f"skriv lengde nummer {i+1}/{self.antallKanter}: ")))
     
     def omkrets(self):
+        """denne funksjonan finna omkretsa tel ein figur"""
         self.o = sum(self.lengde_li)
     
     def visInfo(self):
+        """viser infoen til figura"""
         print(f"Mangekanten har {self.antallKanter} kanter, og har omkretsen {self.o}")
 
 class Rektangen(Mangekant):
