1ff3c8f7bc2969af9bc11e83fa9b5de7b9eb3f9a
jonasber06 <144693393+jonasber06@users.noreply.github.com>
2024-10-31 14:30:45 +0100

    rektangel definert

--- mangekant.py
+++ mangekant.py
@@ -4,8 +4,9 @@ class Mangekant:
         self.lengde_li = lengde_li
     
     def angiLengder(self):
+        print("")
         for i in range(self.antallKanter):
-            self.lengde_li.append(int(input(f"skriv lengde nummer {i}:")))
+            self.lengde_li.append(int(input(f"skriv lengde nummer {i+1}/{self.antallKanter}: ")))
     
     def omkrets(self):
         self.o = sum(self.lengde_li)
@@ -13,17 +14,25 @@ class Mangekant:
     def visInfo(self):
         print(f"Mangekanten har {self.antallKanter} kanter, og har omkretsen {self.o}")
 
+class Rektangen(Mangekant):
+    def __init__(self, antallKanter: int, lengde_li: list):
+        super().__init__(antallKanter, lengde_li)
+    
 liste = []
 
 a = Mangekant(5,[])
 a.angiLengder()
 liste.append(a)
 
-b = Mangekant(5,[])
+b = Mangekant(7,[])
 b.angiLengder()
 liste.append(b)
 
-c = Mangekant(5,[])
+c = Mangekant(3,[])
 c.angiLengder()
 liste.append(c)
+
+for r in liste:
+    r.omkrets()
+    r.visInfo()
 
